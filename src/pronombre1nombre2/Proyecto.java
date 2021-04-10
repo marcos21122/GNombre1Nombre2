f0a8dc9babe2eda17756f383d7af0cc32d10e058
marcos <marcos@192.168.1.37>
2021-04-10 19:38:18 +0200

    marce

---
+++ src/pronombre1nombre2/Proyecto.java
@@ -0,0 +1,16 @@
+package pronombre1nombre2;
+
+
+public class Proyecto {
+
+		
+		
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Marce apruebame");
+		 System.out.println("Porfavor que soy buen chaval");
+		 System.out.println("Marce te queremos marce quedate");
+	}
+
+}
